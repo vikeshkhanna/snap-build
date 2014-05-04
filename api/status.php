d2fdc5d62d8057985226ccb9de572750abd6362f
Vikesh Khanna <khanna.vikesh@gmail.com>
2014-05-04 02:35:54 -0700

    Updating the homepage

--- api/status.php
+++ api/status.php
@@ -16,11 +16,13 @@ foreach($PROJECTS as $project => $label) {
 	for($i=0; $i<count($rows); $i++) {
 		$rows[$i]["tstart"] = date('Y-m-d H:i:s', $rows[$i]["tstart"]);
 		$rows[$i]["tend"] = date('Y-m-d H:i:s', $rows[$i]["tend"]);
-		$rows[$i]["key"] = $project;
 	}
 
-	$projectStatus[$label] = $rows;
+	$retVal["label"] = $label;
+	$retVal["rows"] = $rows;
+	$projectStatus[$project] = $retVal;
 }
+
 $db->commit();
 echo json_encode($projectStatus);
 ?>
--- index.php
+++ index.php
@@ -15,101 +15,150 @@ $sec = "3000000000";
 		<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
 
 		<script type="text/javascript">
-				var currentActiveTab = null;
 				// Refreshes frequently through an AJAX request.
 				var currentBuildData;
+				var currentActiveProject = null;
 
-				var buildStatusLabel = {
+				// Maps the project key to the li element.
+				var projectElementMap;
+
+				var statusLabelMap = {
 						"0" : "Successful",
 						"-1" : "Queued",
 						"1" : "Failed",
 						"2" : "In Progress"
 				};
 
-				var buildStatusClass = {
+				var statusClassMap = {
 						"0" : "success",
 						"-1" : "queued",
 						"1" : "failed",
-						"2" : "progress"
+						"2" : "inprogress"
 				};
 
 				$(document).ready(function() {
+
 					function refresh(data) {
 						var dataContainer = $("#project-builds");
+						var rows = data["rows"];
+						var label = data["label"];
 						
 						// Clear the table except the first row (heading)
 						while ($("#project-builds tr").length > 1) {
 							$("#project-builds tr:last-child").remove();
 						}
+		
 				
-						for (var i=0; i<data.length; i++) {
-							var row = data[i];
+						for (var i=0; i < rows.length; i++) {
+							var row = rows[i];
 							var buildStatus = parseInt(row['build_status']);
 							var testStatus = parseInt(row['test_status']);
-							var trClass = "failed";
-
-							if (buildStatus === -1 || buildStatus === 2 || testStatus === -1 || testStatus === 2) {
-								trClass = "progress";	
-							} else if (buildStatus === 0 && testStatus === 0) {
-								trClass = "success";
-							}
+							var trClass = getRowClass(buildStatus, testStatus);	
 
 							dataContainer.append(
 								$("<tr class='" + trClass + "' />").append(
 									"<td>" + row["id"] +"</td>" + 
 									"<td>" + row["tstart"] +"</td>" + 
 									"<td>" + row["tend"] +"</td>" + 
-									"<td class='" + buildStatusClass[row['build_status']] + "'>" + buildStatusLabel[row["build_status"]] +"</td>" + 
-									"<td class='" + buildStatusClass[row['test_status']] + "'>" + buildStatusLabel[row["test_status"]] +"</td>" + 
+									"<td class='" + statusClassMap[row['build_status']] + "'>" + statusLabelMap[row["build_status"]] +"</td>" + 
+									"<td class='" + statusClassMap[row['test_status']] + "'>" + statusLabelMap[row["test_status"]] +"</td>" + 
 									"<td><a target='_blank' href='logs/" + row["key"] + "/" + row["logs"] +"'>" + row["logs"] + "</a></td>"
 								)
 							);
 						}
+
+					}
+
+					/*
+						* Returns the cumulative status of the row based on build and test.
+						* Current behaviour is to return failed if either test or build failed. 
+						* 'progress' in case either is in progress.
+						* 'success' if both build and test succeeded.
+					 */
+					function getRowClass(buildStatus, testStatus) {
+						var trClass = "failed";
+						if (buildStatus === -1 || buildStatus === 2 || testStatus === -1 || testStatus === 2) {
+							trClass = "inprogress";	
+						} else if (buildStatus === 0 && testStatus === 0) {
+							trClass = "success";
+						}
+						return trClass;
 					}
 
 					/* 
-						* Returns the tab click handler. Necessary because JS has only function scope. 
-						* Anonmyous functions inside for loops are trouble.
+					 * Returns the tab click handler. Necessary because JS has only function scope. 
+					 * Anonmyous functions inside for-loops are trouble.
 					 */
-					function createTabHandler(label) {
+					function createTabHandler(projectName) {
 						return function(e) {
 							// Refresh the data in the table.
-							refresh(currentBuildData[label]);
-							$(currentActiveTab).removeClass('active');
-							currentActiveTab = $(e.target).parent();
-							$(currentActiveTab).addClass('active');
+							refresh(currentBuildData[projectName]);
+
+							// Change the current active project.
+							var currentActiveTabSelector = "#" + currentActiveProject;
+							$(currentActiveTabSelector).removeClass('active');
+							currentActiveProject = projectName;
+
+							currentActiveTabSelector = "#" + currentActiveProject;
+							$(currentActiveTabSelector).addClass('active');
 						}
 					}
 
-					// Get status of all projects.
-					$.ajax({
-						url : "api/status.php",
-						dataType : "json",
-					})
-					.done(function(response) {
-						// Update currentBuildData.
-						currentBuildData = response;
-						var labelArray = [];
+					function fetchAndRefreshData() {
+						// Get status of all projects.
+						$.ajax({
+							url : "api/status.php",
+							dataType : "json",
+						})
+						.done(function(response) {
+							// Update currentBuildData.
+							currentBuildData = response;
+							var projectNames = [];
 
-						// true only for the first request.
-						if (currentActiveTab === null) {
-							for(label in response) {
-								labelArray.push(label);
-								$("#project-tabs").append(
-									$("<li />").append(
-										$("<a />")
-											.attr("data-toggle", "tab")
-											.html(label)
-											.click(createTabHandler(label))
-										)
-								);
+							// True only for the first request.
+							if (currentActiveProject === null) {
+								// Update the tabs with project names and attach handlers to click events.
+								for(projectName in response) {
+									projectNames.push(projectName);
+									$("#project-tabs").append(
+										$("<li id='" + projectName + "'/>").append(
+											$("<a />")
+												.attr("data-toggle", "tab")
+												.html(response[projectName]["label"])
+												.click(createTabHandler(projectName))
+											)
+										);
+								}
+
+								// Set the first tab as the active element and the first project as the currentActiveProject.
+								var el = $("#project-tabs li:first");
+								el.attr('class', 'active');
+								currentActiveProject = projectNames[0];
 							}
-							var el = $("#project-tabs li:first");
-							el.attr('class', 'active');
-							currentActiveTab = el;
-							refresh(response[labelArray[0]]);
-						}
-					});
+
+								// Create project summary for latest build and test status.
+								// Clear the project summary div.
+								$("#project-summary-container").empty();
+
+								// For each project, add the the latest build summary to the project-summary-container div.
+								for(projectName in response) {
+									var latestBuildStatus = parseInt(response[projectName]["rows"][0]["build_status"])
+									var latestTestStatus = parseInt(response[projectName]["rows"][0]["test_status"]);
+									var label = response[projectName]["label"];
+
+									$("<li />")
+										.attr('class', 'project-summary ' + getRowClass(latestBuildStatus, latestTestStatus))
+										.append("<h3>" + label + "</h3>")
+										.append("<span>Build " + statusLabelMap[latestBuildStatus] + "</span>")
+										.append("<span>Test  " + statusLabelMap[latestTestStatus] + "</span>")
+										.appendTo("#project-summary-container");
+								}
+							refresh(response[currentActiveProject]);
+						});
+					}
+
+					setInterval(fetchAndRefreshData, 30000);
+					fetchAndRefreshData();
 				});
 		</script>
 	</head>
@@ -117,32 +166,25 @@ $sec = "3000000000";
 	<body>
 		<div class="container">
 			<div class="header"><h3 class="text-muted">SNAP Build dashboard<h3></div>
-						<div class="bs-callout failed project-summary failed">
-							<h3>snap</h3>
-							<span class="success">Build Successful</span>	
-							<span class="failed">Test Failed</span>	
-						</div>
-
-						<div class="bs-callout project-summary success">
-							<h3>snapr</h3>
-							<span class="failed">Build Successful</span>	
-							<span class="failed">Test Successful</span>	
-						</div>
-
-				<ul class="nav nav-tabs" id="project-tabs">
+				<ul id="project-summary-container" class="list-inline">
 				</ul>
 
-				<div class="builds">
-					<table class="table" id="project-builds">
-						<tr>
-							<th>Build Number</th>
-							<th>Start Time</th>
-							<th>End Time</th>
-							<th>Build Status</th>
-							<th>Test Status</th>
-							<th>Logs</th>
-						</tr>
-					</table>
+				<div id="project-builds-nav">
+					<ul class="nav nav-tabs" id="project-tabs">
+					</ul>
+
+					<div id="project-builds-container">
+						<table class="table" id="project-builds">
+							<tr>
+								<th>Build Number</th>
+								<th>Start Time</th>
+								<th>End Time</th>
+								<th>Build Status</th>
+								<th>Test Status</th>
+								<th>Logs</th>
+							</tr>
+						</table>
+					</div>
 				</div>
 			<div class="footer">&copy Stanford SNAP</div>
 		</div>
